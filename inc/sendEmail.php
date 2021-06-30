addd47021e15f47c828374dc1bd1639c9f5e2f11
hackathonlukky <spambotofficial44@gmail.com>
2021-07-01 00:24:51 +0530

    Filled in Content

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-// Replace this with your own email address
+
 $siteOwnersEmail = 'lakshay0804@gmail.com';
 
 
@@ -11,33 +11,33 @@ if($_POST) {
     $subject = trim(stripslashes($_POST['contactSubject']));
     $contact_message = trim(stripslashes($_POST['contactMessage']));
 
-    // Check Name
+
     if (strlen($name) < 2) {
         $error['name'] = "Please enter your name.";
     }
-    // Check Email
+
     if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
         $error['email'] = "Please enter a valid email address.";
     }
-    // Check Message
+
     if (strlen($contact_message) < 15) {
         $error['message'] = "Please enter your message. It should have at least 15 characters.";
     }
-    // Subject
+
     if ($subject == '') { $subject = "Contact Form Submission"; }
 
 
-    // Set Message
+
     $message .= "Email from: " . $name . "<br />";
     $message .= "Email address: " . $email . "<br />";
     $message .= "Message: <br />";
     $message .= $contact_message;
     $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
 
-    // Set From: header
+
     $from =  $name . " <" . $email . ">";
 
-    // Email Headers
+
     $headers = "From: " . $from . "\r\n";
     $headers .= "Reply-To: ". $email . "\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
@@ -46,13 +46,13 @@ if($_POST) {
 
     if (!$error) {
 
-        ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+        ini_set("sendmail_from", $siteOwnersEmail);
         $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
         if ($mail) { echo "OK"; }
         else { echo "Something went wrong. Please try again."; }
 
-    } # end if - no validation error
+    }
 
     else {
 
@@ -62,7 +62,7 @@ if($_POST) {
 
         echo $response;
 
-    } # end if - there was a validation error
+    }
 
 }
 
